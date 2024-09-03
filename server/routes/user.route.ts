60249b5e7a973b60347784be4d1e405d495c22c8
agrpranjal07 <myselfpranjal2005@gmail.com>
2024-09-03 21:31:53 +0530

    Generate new Access token, user Social authentication, user info

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import { activateUser, loginUser, logoutUser, registrationUser } from "../controllers/user.controller";
+import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken } from "../controllers/user.controller";
 import { isAuthenticated } from "../middleware/auth";
-const router = express.Router();
+const userRouter = express.Router();
 
-router.post("/register", registrationUser);
-router.post("/activate-user", activateUser);
-router.post("/login",loginUser);
-router.get("/logout", isAuthenticated ,logoutUser);
-export default router;
+userRouter.post("/register", registrationUser);
+
+userRouter.post("/activate-user", activateUser);
+
+userRouter.post("/login",loginUser);
+
+userRouter.get("/logout", isAuthenticated ,logoutUser);
+
+userRouter.get("/refresh",updateAccessToken)
+
+userRouter.get("/me",isAuthenticated, getUserInfo)
+
+userRouter.post("/social-auth",socialAuth)
+
+export default userRouter;
