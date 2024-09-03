64ff08af8959020922d1cd162f01b6aca7f57280
agrpranjal07 <myselfpranjal2005@gmail.com>
2024-09-03 20:29:22 +0530

    Add user protected and authorization middleware

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -6,6 +6,7 @@ import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import jwt, { Secret } from "jsonwebtoken";
 import sendEmail from "../utils/sendMail";
 import { sendToken } from "../utils/jwt";
+import { redis } from "../utils/redis";
 
 //register user
 interface IRegistrationBody {
@@ -162,6 +163,9 @@ export const logoutUser = CatchAsyncError(
     try {
       res.cookie("access_token", "", { maxAge: 1 });
       res.cookie("refresh_token", "", { maxAge: 1 });
+      const userId = String(req.user?._id) || "";
+      redis.del(userId);
+
       res.status(200).json({
         success: true,
         message: "Logged out successfully",
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { activateUser, loginUser, logoutUser, registrationUser } from "../controllers/user.controller";
+import { isAuthenticated } from "../middleware/auth";
 const router = express.Router();
 
 router.post("/register", registrationUser);
 router.post("/activate-user", activateUser);
-router.post("/login", loginUser);
-router.get("/logout", logoutUser);
+router.post("/login",loginUser);
+router.get("/logout", isAuthenticated ,logoutUser);
 export default router;
